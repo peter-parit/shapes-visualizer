0e2f7b6920e94720201daf1f705dc40972cf60eb
peterparit <144830000+peter-parit@users.noreply.github.com>
2023-11-15 21:03:37 +0700

    add main files

---
+++ shapes.py
@@ -0,0 +1,104 @@
+import pygame as pg
+import numpy as np
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+size = 80
+transX = 250
+transY = 250
+
+
+class Cube:
+    def __init__(self):
+        # https://www.malinc.se/math/linalg/rotatecubeen.php
+        self.vertices = size * np.array([
+            [1, 1, 1],
+            [1, -1, 1],
+            [-1, -1, 1],
+            [-1, 1, 1],
+            [1, 1, -1],
+            [1, -1, -1],
+            [-1, -1, -1],
+            [-1, 1, -1]
+        ])
+        self.lines = [
+            (0, 1),
+            (1, 2),
+            (2, 3),
+            (3, 0),
+            (4, 5),
+            (5, 6),
+            (6, 7),
+            (7, 4),
+            (0, 4),
+            (1, 5),
+            (2, 6),
+            (3, 7)
+        ]
+
+
+class Prism:
+    def __init__(self):
+        self.vertices = size * np.array([
+            [1, 1, 0],
+            [1, -1, 0],
+            [-1, -1, 0],
+            [-1, 1, 0],
+            [1, 0, 1],
+            [-1, 0, 1]
+        ])
+        self.lines = [
+            (0, 1),
+            (1, 2),
+            (2, 3),
+            (3, 0),
+            (4, 5),
+            (4, 1),
+            (4, 0),
+            (5, 2),
+            (5, 3)
+        ]
+
+
+def update(shape, r, locations, screen):
+    for point in shape.vertices:
+        # R = calc_r(thX, thY, thZ)  # update rotation
+        rotation = np.matmul(point, r)
+        px, py = rotation[0], rotation[2]
+
+        locations.append((px + transX, py + transY))
+        pg.draw.circle(screen, RED, ((px + transX), py + transY), 6)
+
+    for pos in shape.lines:
+        pg.draw.line(screen, WHITE, locations[pos[0]], locations[pos[1]], 4)
+
+
+def show_axis(r, screen):  # recheck coordinates
+    locations = []
+    points = size * np.array([
+        [2, 0, 0],
+        [0, 2, 0],  # Z lmao
+        [0, 0, 2],
+        [0, 0, 0]
+    ])
+    lines = [
+        (3, 0),
+        (3, 1),
+        (3, 2)
+    ]
+    colors = [RED, GREEN, BLUE]
+    i = 0
+    for point in points:
+        # R = calc_r(thX, thY, thZ)  # update rotation
+        rotation = np.matmul(point, r)
+        px, py = rotation[0], rotation[2]
+
+        locations.append((px + transX, py + transY))
+
+    for pos in lines:
+        pg.draw.line(screen, colors[i], locations[pos[0]], locations[pos[1]], 4)
+        i += 1
---
+++ shapes_visualizer.py
@@ -0,0 +1,122 @@
+import pygame as pg
+from pygame.locals import *
+from sys import exit
+import numpy as np
+from math import sin, cos
+import shapes
+
+pg.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+screen_size = (700, 500)
+size = 80
+transX = 250
+transY = 250
+
+pg.display.set_caption('3D Shapes Visualizer')
+screen = pg.display.set_mode(screen_size)
+axis_legend = pg.image.load("images/axis.png").convert_alpha()
+axis_legend = pg.transform.scale(axis_legend, (200, 100))
+font = pg.font.Font('freesansbold.ttf', 16)
+keys = font.render('[a]: show/hide axes, [arrow keys]: rotate plane, [s]: change shape', True, WHITE, BLACK)
+keys_rect = keys.get_rect()
+clock = pg.time.Clock()
+
+
+# matrices
+def calc_r(x, y, z):
+    rx = np.array([
+        [1, 0, 0],
+        [0, cos(x), -sin(x)],
+        [0, sin(x), cos(x)]
+    ])
+    ry = np.array([
+        [cos(y), 0, sin(y)],
+        [0, 1, 0],
+        [-sin(y), 0, cos(y)]
+    ])
+    rz = np.array([
+        [cos(z), -sin(z), 0],
+        [sin(z), cos(z), 0],
+        [0, 0, 1]
+    ])
+    r = np.matmul(np.matmul(rz, ry), rx)  # R(z)R(y)R(x)
+    return r
+
+
+# starting angle values for coordinate plane
+thX = 3
+thY = 0
+thZ = 1.8
+
+s = 1  # shapes
+axis = -1  # axis
+changeZ, changeX = 0, 0
+speedZ, speedX = 0, 0
+myCube = shapes.Cube()
+myPrism = shapes.Prism()
+while True:
+    print(thZ)
+    screen.fill(BLACK)
+    screen.blit(keys, (10, 475))
+    screen.blit(axis_legend, (470, 20))
+
+    for event in pg.event.get():
+        if event.type == QUIT:
+            pg.quit()
+            exit()
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pg.quit()
+                exit()
+            elif event.key == K_LEFT:
+                speedZ += 0.003
+            elif event.key == K_RIGHT:
+                speedZ -= 0.003
+            elif event.key == K_DOWN:
+                speedX -= 0.003
+            elif event.key == K_UP:
+                speedX += 0.003
+            elif event.key == K_a:
+                axis = -axis  # toggle axis
+            elif event.key == K_s:
+                s = -s
+        elif event.type == KEYUP:
+            if event.key in (K_LEFT, K_RIGHT):
+                speedZ = 0
+            elif event.key in (K_UP, K_DOWN):
+                speedX = 0
+
+    # speed
+    changeX += speedX
+    changeZ += speedZ
+
+    # decelerate
+    if speedX == 0:
+        changeX *= 0.9
+    if speedZ == 0:
+        changeZ *= 0.9
+
+    # update angle
+    thZ += changeZ
+    thX += changeX
+
+    # update rotation
+    locations = []
+    R = calc_r(thX, thY, thZ)
+    if s == 1:
+        shapes.update(myCube, R, locations, screen)
+    else:
+        shapes.update(myPrism, R, locations, screen)
+
+    # axis
+    if axis == 1:
+        shapes.show_axis(R, screen)
+
+    pg.display.update()
+    clock.tick(60)
